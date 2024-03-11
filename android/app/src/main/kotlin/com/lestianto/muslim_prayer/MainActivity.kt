9fb0fa6ec4fb7986d93e67f65d4ba6bc24800e4a
andi-lestianto <andi7les@gmail.com>
2024-03-11 16:02:15 +0700

    init

---
+++ android/app/src/main/kotlin/com/lestianto/muslim_prayer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lestianto.muslim_prayer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/theme/color_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  static const Color primaryGreen = Color(0xFF014327);
+  static const Color secondaryGreen = Color(0xFF01351E);
+  static const Color pureWhite = Color(0xFFFFFFFF);
+  static const Color whiteGray = Color(0xFFF7F7F7);
+  static const Color pureBlack = Color(0xFF000000);
+  static const Color darkGray = Color(0xFF444444);
+  static const Color lightGray = Color(0xFFA1A1A1);
+  static const Color pureYellow = Color(0xFFFAC022);
+}
